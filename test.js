370b2e181a78f55412e6534ecf093929420b4295
Devin Chen <d3vin.chen@gmail.com>
2020-07-14 00:45:39 +0800

    test for content

--- test.js
+++ test.js
@@ -1,9 +1,13 @@
-describe('Google', () => {
+describe("Google", () => {
   beforeAll(async () => {
-    await page.goto('https://github.com');
+    jest.setTimeout(30000);
+    await page.goto("https://xxd3vin.github.io/about/");
   });
 
-  it('should be titled "GitHub"', async () => {
-    await expect(page.title()).resolves.toMatch('GitHub');
+  it("should have Grab keyword", async () => {
+    // API for page object
+    // https://github.com/puppeteer/puppeteer/blob/v5.1.0/docs/api.md#class-page
+    // await expect(page.title()).resolves.toMatch("About");
+    await expect(page.content()).resolves.toMatch("Grab");
   });
 });
